1c43aeacd05ef2c8cbd7304182acddcde4e7f53c
Evan Tobin <evantobin91384@gmail.com>
2024-05-06 22:01:22 -0700

    Fixed all of the issues with the Arduino code

--- nano-bluetooth/nano-bluetooth.ino
+++ nano-bluetooth/nano-bluetooth.ino
@@ -1,159 +1,177 @@
 #include <SPI.h>
 #include <ArduinoBLE.h>
 
+#define RDY_PIN         D2
+#define BLE_ROLE_PIN    D3
 #define BUFFER_SIZE     64
 #define SPI_FREQUENCY   1000000
+
+typedef enum {
+  NO_ERRORS       = 0x00,
+  ERROR_SPI       = 0x01,
+  ERROR_BLUETOOTH = 0x02
+} ERROR_TYPE;
+
+enum {
+  HEADER = 0x22,
+  WRITE  = 0x01,
+  READ   = 0x02
+} COMMAND_TYPE;
+
+typedef enum {
+  BLE_PERIPHERAL = 0,
+  BLE_CENTRAL = 1
+} BLE_ROLE;
 
 const char* serviceUuid = "12345678-1234-1234-1234-123456789012";
 const char* characteristicUuid = "87654321-4321-4321-4321-210987654321";
 
-BLEService bleService(serviceUuid);
-BLEByteCharacteristic bleCharacteristic(characteristicUuid, BLERead | BLEWrite | BLENotify);
+volatile bool RECEIVING_DATA_SPI = false;
+BLE_ROLE role;
+static byte BluetoothIncoming[BUFFER_SIZE] = {0x40};
+static byte BluetoothIncoming_Size = 1;
 
-bool isCentral = false;
-volatile boolean RECEIVING_DATA = false;
+BLEService peripheralService(serviceUuid);
+BLECharacteristic peripheralCharacteristic(characteristicUuid, BLERead | BLEWrite | BLENotify, 16);
+BLECharacteristic centralCharacteristic;
 
-byte BluetoothIncoming[BUFFER_SIZE];
-byte BluetoothIncoming_Size = 0;
+void slaveSelectAsserted() {
+  RECEIVING_DATA_SPI = true;
+}
 
+void SetupPeripheral() {
+  BLE.setLocalName("Arduino Peripheral");
+  BLE.setAdvertisedService(peripheralService);
+  peripheralService.addCharacteristic(peripheralCharacteristic);
+  BLE.addService(peripheralService);
+  peripheralCharacteristic.setEventHandler(BLEWritten, OnBluetoothReceived);
+  
+  Serial.println("Peripheral setup complete, advertising service.");
+  BLE.advertise(); 
+  while (true) {
+    delay(1); // Wait for connection
+
+    if(BLE.connected())
+    {
+      break;
+    }
+    Serial.println("Not connected to central.");
+  }
+
+  digitalWrite(RDY_PIN, HIGH); // Indicate ready when connected
+  Serial.println("Peripheral connected.");
+}
+
+void SetupCentral() {
+  pinMode(RDY_PIN, OUTPUT);
+  digitalWrite(RDY_PIN, LOW);  // Start with RDY_PIN low to indicate not ready
+
+  BLE.begin();
+  BLE.setLocalName("Arduino Central");
+  Serial.println("Scanning for BLE peripherals...");
+
+  while (true) {
+
+    BLEDevice peripheral;
+
+    Serial.println("- Discovering peripheral device...");
+
+    do
+    {
+      BLE.scanForUuid(serviceUuid);
+      peripheral = BLE.available();
+    } while (!peripheral);
+
+    Serial.println("* Peripheral device found!");
+    Serial.print("* Device MAC address: ");
+    Serial.println(peripheral.address());
+    Serial.print("* Device name: ");
+    Serial.println(peripheral.localName());
+    Serial.print("* Advertised service UUID: ");
+    Serial.println(peripheral.advertisedServiceUuid());
+    Serial.println(" ");
+    BLE.stopScan();
+    
+    Serial.println("- Connecting to peripheral device...");
+
+    if (peripheral.connect()) {
+      Serial.println("* Connected to peripheral device!");
+      Serial.println(" ");
+    } else {
+      Serial.println("* Connection to peripheral device failed!");
+      Serial.println(" ");
+      continue;
+    }
+
+    Serial.println("- Discovering peripheral device attributes...");
+    if (peripheral.discoverAttributes()) {
+      Serial.println("* Peripheral device attributes discovered!");
+      Serial.println(" ");
+    } else {
+      Serial.println("* Peripheral device attributes discovery failed!");
+      Serial.println(" ");
+      peripheral.disconnect();
+      continue;
+    }
+
+    centralCharacteristic = peripheral.characteristic(characteristicUuid);
+      
+    if (!centralCharacteristic) {
+      Serial.println("* Peripheral device does not have gesture_type characteristic!");
+      peripheral.disconnect();
+      continue;
+    }
+
+    centralCharacteristic.setEventHandler(BLEWritten, OnBluetoothReceived);
+
+    break;
+  }
+
+  digitalWrite(RDY_PIN, HIGH); // Indicate ready when connected
+  Serial.println("Central connected.");
+}
 
 void setup() {
+  pinMode(BLE_ROLE_PIN, INPUT);
+  pinMode(RDY_PIN, OUTPUT);
+  pinMode(SS, INPUT_PULLUP);
+
+  // delay(3000);
   Serial.begin(9600);
-  delay(1000);  // Delay 1 second to get the serial ready
-
-
-  // Setup BLE
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(D3, INPUT);
-  isCentral = digitalRead(D3);
 
   if (!BLE.begin()) {
     Serial.println("Starting BLE failed!");
     while (1);
   }
 
-  if (isCentral) {
-    Serial.println("Configured as Central");
-    setupCentral();
-  } else {
-    Serial.println("Configured as Peripheral");
-    setupPeripheral();
+  role = (BLE_ROLE)digitalRead(BLE_ROLE_PIN);
+  
+  switch(role) {
+    case BLE_PERIPHERAL:
+      SetupPeripheral();
+      break;
+    case BLE_CENTRAL:
+      SetupCentral();
+      break;
   }
 
-  // Setup SPI after establishing connection
-  pinMode(D2, OUTPUT);
-  digitalWrite(D2, HIGH);
-  pinMode(SS, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(SS), slaveSelectAsserted, FALLING);
   SPI.begin();
   SPI.beginTransaction(SPISettings(SPI_FREQUENCY, MSBFIRST, SPI_MODE0));
 }
 
 void loop() {
-  BLE.poll();
-
-  if (RECEIVING_DATA) {
+  if (RECEIVING_DATA_SPI) {
     ERROR_TYPE error = ProcessReceivingData();
-    RECEIVING_DATA = false;
+    RECEIVING_DATA_SPI = false;
     if (error != NO_ERRORS) {
-      digitalWrite(D2, LOW);
+      digitalWrite(RDY_PIN, LOW);
     }
   }
+
+  BLE.poll();
 }
 
-void setupCentral() {
-  BLE.setLocalName("Arduino Central");
-  if (!centralConnectionProcedure()) {
-    Serial.println("Failed to establish a connection. Reset needed.");
-    while(1); // Stop execution here
-  }
-}
-
-bool centralConnectionProcedure() {
-    Serial.println("Starting scan for BLE peripherals...");
-    BLE.scanForUuid(serviceUuid);
-
-    int attempt = 0;
-    const int maxAttempts = 100;  // Maximum number of attempts
-
-    while (attempt < maxAttempts) {
-        BLEDevice peripheral = BLE.available();
-        if (peripheral) {
-            Serial.println("Found a peripheral, attempting to connect...");
-            if (peripheral.connect()) {
-                Serial.println("Connected to peripheral");
-                delay(1000); // Wait for the connection to stabilize
-
-                if(peripheral.serviceCount())
-                {
-                  Serial.println("Serivces found...");
-                }
-                else
-                {
-                  Serial.println("No services found...");
-                }
-
-                // Manually initiate service discovery
-                if (peripheral.discoverAttributes()) {
-                  Serial.println("Attributes discovered");
-                } else {
-                  Serial.println("Attribute discovery failed!");
-                  peripheral.disconnect();
-                  continue;
-                }
-                delay(1000); // Wait to ensure discovery completes
-                if (peripheral.discoverService(serviceUuid)) {
-                    BLECharacteristic charac = peripheral.characteristic(characteristicUuid);
-                    if (charac) {
-                        Serial.println("Characteristic found");
-                        BLE.stopScan();
-                        return true;
-                    } else {
-                        Serial.println("Characteristic not found");
-                    }
-                } else {
-                    Serial.println("Service not found");
-                    peripheral.disconnect();
-                    continue;
-                }
-            } else {
-                Serial.println("Failed to connect, retrying...");
-            }
-        } else {
-            Serial.println("No suitable peripherals found this cycle.");
-        }
-        attempt++;
-        delay(1000);  // Delay 1 second before next attempt
-    }
-    BLE.stopScan();
-    return false;  // Connection or characteristic discovery failed after retries
-}
-
-
-
-void setupPeripheral() {
-  BLE.setEventHandler(BLEDisconnected, onDisconnect);
-  BLE.setEventHandler(BLEConnected, onConnect);
-  BLE.setLocalName("Arduino Peripheral");  // Set the local name
-  bleService.addCharacteristic(bleCharacteristic);  // Add the characteristic to the service
-  BLE.addService(bleService);  // Add the service
-  BLE.setAdvertisedService(bleService);    // Add the service UUID to the advertising data
-  BLE.advertise();
-  Serial.println("Advertising started...");
-}
-
-void onDisconnect(BLEDevice central) {
-    Serial.println("Disconnected from central, restarting advertising");
-    BLE.advertise();
-}
-
-void onConnect(BLEDevice central) {
-    Serial.println("Connected to central!");
-}
-
-void slaveSelectAsserted() {
-  RECEIVING_DATA = true;
-}
 
 ERROR_TYPE ProcessReceivingData() {
   byte header_byte = SPI.transfer(0x00);
@@ -161,8 +179,9 @@ ERROR_TYPE ProcessReceivingData() {
     Serial.println("Error Received from header...");
     return ERROR_SPI;
   }
+
   byte command_byte = SPI.transfer(0x22);
-  switch(command_byte) {
+  switch (command_byte) {
     case WRITE: {
       byte write_length = SPI.transfer(0x00);
       byte bluetooth_outgoing[write_length];
@@ -182,52 +201,50 @@ ERROR_TYPE ProcessReceivingData() {
     }
     default:
       return ERROR_SPI;
-  }  
+  }
   return NO_ERRORS;
 }
 
+
+
+void OnBluetoothReceived(BLEDevice central, BLECharacteristic characteristic) 
+{
+  const byte* dataReceived = characteristic.value();
+  size_t length = characteristic.valueLength();
+  Serial.println("Received: ");
+  for (size_t i = 0; i < length; i++) 
+  {
+    BluetoothIncoming[BluetoothIncoming_Size++] = dataReceived[i];
+    Serial.println(dataReceived[i]);
+    Serial.println(", ");
+  }
+  Serial.println("\n");
+}
+
+
+
 void SendBluetoothData(byte length, byte* data) {
-  if (BLE.connected() && bleCharacteristic.canWrite()) {
-    bool success = bleCharacteristic.writeValue(data[0]);
+  bool success = false;
+  if (BLE.connected()) {
+    if (role == BLE_PERIPHERAL) {
+      success = peripheralCharacteristic.writeValue(data, length);
+    } else if (role == BLE_CENTRAL) {
+      success = centralCharacteristic.writeValue(data, length);
+    }
+
+    Serial.print("Sent ");
+    for(int i = 0; i < length; i++)
+    {
+      Serial.print(data[i]);
+    }
+    Serial.println(".");
+
     if (success) {
       Serial.println("Data sent successfully");
     } else {
       Serial.println("Failed to send data");
     }
   } else {
-    Serial.println("Not connected to a peripheral or characteristic is not writable");
+    Serial.println("Not connected.");
   }
 }
-
-void OnBluetoothRead(BLEDevice central, BLECharacteristic characteristic) {
-  Serial.println("Central device read characteristic");
-  // Additional handling code here
-}
-
-void OnBluetoothReceived(BLEDevice central, BLECharacteristic characteristic) {
-  Serial.println("RECEIVED");
-  const byte* dataReceived = characteristic.value();
-  size_t length = characteristic.valueLength();
-  for (size_t i = 0; i < length; i++) {
-    BluetoothIncoming[BluetoothIncoming_Size++] = dataReceived[i];
-  }
-}
-
-
-
-
-
-
-
-
-typedef enum {
-  NO_ERRORS       = 0x00,
-  ERROR_SPI       = 0x01,
-  ERROR_BLUETOOTH = 0x02
-} ERROR_TYPE;
-
-enum {
-  HEADER = 0x22,
-  WRITE  = 0x01,
-  READ   = 0x02
-} COMMAND_TYPE;
